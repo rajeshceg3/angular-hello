b307a4d42b862434a8bb183872a291db2dec700a
rajeshceg3 <rajeshceg3@gmail.com>
2023-02-05 07:06:08 +0530

    4 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -17,7 +17,7 @@ constructor( private route : ActivatedRoute){
 ngOnInit(){
   const routeParams = this.route.snapshot.paramMap;
   const productId = Number( routeParams.get('productId'));
-  this.product = products.find((product)=> product.id === productId );
+  this.product = products.find(product => product.id === productId );
 }
 
 }
