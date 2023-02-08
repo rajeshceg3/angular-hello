f55cf6aa2702f86a6015e7e2c5682401f3aa7d47
rajeshceg3 <rajeshceg3@gmail.com>
2023-02-08 11:49:08 +0530

    3 files modified, 4 added

---
+++ src/app/shipping/shipping.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs'
+import { CartService } from '../cart.service'
+
+@Component({
+  selector: 'app-shipping',
+  templateUrl: './shipping.component.html',
+  styleUrls: ['./shipping.component.css']
+})
+export class ShippingComponent {
+  shippingCosts !: Observable<{type:String, price:Number}[]>;
+  
+  constructor( private cartService:CartService){
+    this.shippingCosts = cartService.getShippingPrices();
+  }
+
+
+
+}
