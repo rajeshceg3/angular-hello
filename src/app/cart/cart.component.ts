05f923c53e4d6af504b5968c4c86ba57cffe5c50
rajeshceg3 <rajeshceg3@gmail.com>
2023-02-08 11:53:14 +0530

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms'
 import { CartService } from '../cart.service'
 
 @Component({
@@ -8,7 +9,10 @@ import { CartService } from '../cart.service'
 })
 export class CartComponent {
   items = this.cartService.getItems();
-  constructor( private cartService:CartService){
+  constructor(
+     private cartService:CartService,
+     private formBuilder:FormBuilder
+     ){
 
   }
 
